691ba3c5eb31d5c4ed2307e536f00bf918c2d3bc
russelltrupiano <russelltrupiano@gmail.com>
2015-02-12 14:52:22 -0500

    made dynamic for cycles

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+var validator = require('validator');
 
 
 
-function ShuffleSongs() {
-    var files = fs.readdirSync(path.join(__dirname, "/../public/tracks"))
+function ShuffleSongs(cycle) {
+    var files = fs.readdirSync(path.join(__dirname, "/../public/tracks/cycle" + cycle))
                     .filter(function(file) {
-                        return file !== ".DS_Store";
+                        return (file !== ".DS_Store" && file !== "README.md");
                     });
 
     var indicies = [];
@@ -25,6 +26,9 @@ function ShuffleSongs() {
 
     for (var i = 0; i < indicies.length; i++) {
         var songData = files[indicies[i]].split(" - ");
+
+        console.log(songData);
+
         songData[1] = songData[1].split('.')[0];
         trackData[i] = songData;
     };
@@ -39,12 +43,39 @@ function ShuffleSongs() {
 /* GET home page. */
 router.get('/', function(req, res) {
 
-    var arrayData = ShuffleSongs();
+    var numCycles = fs.readdirSync(path.join(__dirname, "/../public/tracks"))
+                        .filter(function(file) {
+                            return (file.indexOf("cycle") !== -1);
+                        });;
+
+    res.render('index', {
+        numCycles: numCycles
+    });
+});
+
+
+/* GET home page. */
+router.get('/:cycle', function(req, res) {
+
+    var cycle = req.params.cycle;
+
+    console.log(cycle);
+
+    if (!validator.isInt(cycle)) {
+        console.log("Bad param");
+        var numCycles = fs.readdirSync(path.join(__dirname, "/../public/tracks")).length;
+        if (cycle == 0 || cycle > numCycles) {
+            res.render('error');
+        }
+    }
+
+    var arrayData = ShuffleSongs(cycle);
 
     console.log(arrayData.trackData);
 
-    res.render('index', {
-        title: 'Express',
+    res.render('tracks', {
+        title: 'TrackID',
+        cycle: cycle,
         files: arrayData.files,
         indicies: arrayData.indicies,
         trackData: arrayData.trackData
